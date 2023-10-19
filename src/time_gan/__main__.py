d871dbe7567e781974bf1c41614ed2916551ee1a
opsullivan85 <opsullivan85@gmail.com>
2023-10-19 13:46:10 -0400

    setup main

--- src/time_gan/__main__.py
+++ src/time_gan/__main__.py
@@ -3,7 +3,27 @@
 # anything that is meant to be imported by other modules.
 
 import logging
-from src import time_gan
+from src.time_gan import timegan
+from src.data_pre_processing import load_data
 
 logger = logging.getLogger(__name__)
 logger.debug(f"Loading {__name__}")
+
+
+## Newtork parameters
+parameters = dict()
+
+parameters["module"] = "gru"
+parameters["hidden_dim"] = 24
+parameters["num_layer"] = 3
+parameters["iterations"] = 10000
+parameters["batch_size"] = 128
+
+# Get data
+train_X, train_y = load_data()
+
+# Run TimeGAN
+generated_data = timegan(train_X, parameters)
+print("Finish Synthetic Data Generation")
+
+print(generated_data)
