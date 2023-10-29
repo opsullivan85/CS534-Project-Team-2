401344294ee82a520eaf218b05c02ddf5f53bca2
Cameron Schloer <cameronschloer@gmail.com>
2023-10-29 18:28:55 -0400

    Added filter code for only using good boid data

--- src/time_gan/__main__.py
+++ src/time_gan/__main__.py
@@ -4,13 +4,15 @@
 
 import logging
 from src.time_gan import TimeGan
-from src.data_pre_processing import load_timeseries_data, window_size, X_fields_per_boid
+from src.data_pre_processing import load_timeseries_data, filter_fault_type_boids_from_data, window_size, X_fields_per_boid
 
 logger = logging.getLogger(__name__)
 logger.debug(f"Loading {__name__}")
 
 # Get data
+NOT_FAULTY = 0
 train_X, train_y = load_timeseries_data()
+filtered_X, filtered_y = filter_fault_type_boids_from_data(train_X, train_y, NOT_FAULTY)
 
 ## Newtork parameters
 parameters = dict()
@@ -19,12 +21,12 @@ parameters["module"] = "gru"
 parameters["hidden_dim"] = 24
 parameters["num_layer"] = 3
 parameters["batch_size"] = 128
-epoch_size = train_X.shape[0] // parameters["batch_size"]
+epoch_size = filtered_X.shape[0] // parameters["batch_size"]
 parameters["iterations"] = epoch_size * 4
 print(f"{parameters = }")
 
 # Run TimeGAN
-generated_data = TimeGan(train_X, parameters)
+generated_data = TimeGan(filtered_X, parameters)
 print("Finish Synthetic Data Generation")
 
 print(generated_data)
