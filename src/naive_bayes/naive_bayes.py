fddf6aafc7d26337fbb57448465c6539454c571e
opsullivan85 <opsullivan@wpi.edu>
2023-10-30 10:12:18 -0400

    changed code structure to match paper

--- src/naive_bayes/naive_bayes.py
+++ src/naive_bayes/naive_bayes.py
@@ -5,25 +5,38 @@ from src.data_generation import train_path, test_path
 from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
 
 
+def pre_process_data(X, y):
+    y = np.minimum(y, 1)  # convert all values to 0 or 1
+    y = y.ravel()  # flatten y_train
+    X += 1  # make all values positive
+    return X, y
+    
+    
+
+
 def run():
+    # load data
     X_train, y_train = load_data(train_path)
     X_test, y_test = load_data(test_path)
-    y_train = np.minimum(y_train, 1)  # convert all values to 0 or 1
-    y_train = y_train.ravel()  # flatten y_train
-    y_test = np.minimum(y_test, 1)  # convert all values to 0 or 1
-    y_test = y_test.ravel()  # flatten y_train
-    X_train += 1  # make all values positive
-    X_test += 1  # make all values positive
 
+    # pre-process data (make all X values positive, 
+    # convert all y values to 0 or 1)
+    X_train, y_train = pre_process_data(X_train, y_train)
+    X_test, y_test = pre_process_data(X_test, y_test)
+
+    # down sample data so that the number of healthy 
+    # boids is equal to the number of faulty boids
     X_train, y_train = down_sample_data(X_train, y_train)
     X_test, y_test = down_sample_data(X_test, y_test)
 
+    # get model and predict
     model = nb.GaussianNB().fit(X_train, y_train)
     y_pred = model.predict(X_test)
 
     # Calculate accuracy and confusion matrix for the test set
     accuracy = accuracy_score(y_test, y_pred)
     confusion = confusion_matrix(y_test, y_pred)
+    classification = classification_report(y_test, y_pred)
 
     print("Naive Bayes")
     print()
@@ -32,6 +45,6 @@ def run():
     print(confusion)
     print()
     print("Classification Report:")
-    print(classification_report(y_test, y_pred))
+    print(classification)
 
     return model
